83e086d208ebb7aa97431b48a8ec79b8d1dcebcf
HP <20060339@stu.omu.edu.tr>
2023-02-16 21:56:26 +0300

    add the clock

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,14 @@
 let Name = prompt("Lütfen Adınızı Giriniz : ");
-
 let myName = document.querySelector("#myName");
-
 myName.innerHTML = `${myName.innerHTML} ${Name}`;
 
+let myClock = document.querySelector("#myClock");
 
+let date = new Date();
+let day = date.getDay();
+let haurs = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+
+myClock.innerHTML = ` ${haurs} : ${min} : ${sec} `
